95a09b9a1595cb63f91b7ae0895c6b1ddbaa95db
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-02-13 23:20:07 +0000

    Centering The Map on A Campground

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,8 +1,11 @@
 mapboxgl.accessToken = mapToken;
 console.log(center);
+center = center || [-74.5, 40];
 const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/mapbox/streets-v11",
-    center: center || [-74.5, 40],
+    center: center,
     zoom: 4,
 });
+
+var marker = new mapboxgl.Marker().setLngLat(center).addTo(map);
