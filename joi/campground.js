49aa1a05efbfff6d3fa010c146aad4371b662dca
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-02-15 15:38:02 +0000

    Cross Site Scripting Sanitizing

--- joi/campground.js
+++ joi/campground.js
@@ -1,13 +1,17 @@
-const joi = require("joi");
+const extension = require("./extensions");
+const BaseJoi = require("joi");
+
+const joi = BaseJoi.extend(extension)
+
 
 module.exports.CampgroundSchema = joi.object({
     campground: joi
         .object({
-            title: joi.string().required(),
+            title: joi.string().required().escapeHTML(),
             price: joi.number().required().min(0),
             // image: joi.string().required(),
-            location: joi.string().required(),
-            description: joi.string().required(),
+            location: joi.string().required().escapeHTML(),
+            description: joi.string().required().escapeHTML(),
         })
         .required(),
     deleteImages: joi.array(),
--- joi/review.js
+++ joi/review.js
@@ -1,10 +1,13 @@
-const joi = require("joi");
+const extension = require("./extensions");
+const BaseJoi = require("joi");
+
+const joi = BaseJoi.extend(extension);
 
 module.exports.ReviewSchema = joi.object({
     review: joi
         .object({
             rating: joi.number().min(0).max(5).required(),
-            body: joi.string().required(),
+            body: joi.string().required().escapeHTML(),
         })
         .required(),
 });
