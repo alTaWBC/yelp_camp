e684b80ebe30485c9e4d093d6a2ccc60bed83077
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-02-15 14:09:20 +0000

    Adding Custom popups

--- models/campground.js
+++ models/campground.js
@@ -17,45 +17,57 @@ ImageSchema.virtual("thumbnail").get(function () {
     return this.url.replace("/upload", "/upload/w_200");
 });
 
-const CampgroundSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    images: [ImageSchema],
-    description: {
-        type: String,
-        required: true,
-    },
-    location: {
-        type: String,
-        required: true,
-    },
-    geometry: {
-        type: {
+// How to include virtuals in Json stringify
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+    {
+        title: {
             type: String,
-            enum: ['Point'],
-            required: true
+            required: true,
         },
-        coordinates: {
-            type: [Number],
-            required: true
-        }
-    },
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-    reviews: [
-        {
+        price: {
+            type: Number,
+            required: true,
+        },
+        images: [ImageSchema],
+        description: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        geometry: {
+            type: {
+                type: String,
+                enum: ["Point"],
+                required: true,
+            },
+            coordinates: {
+                type: [Number],
+                required: true,
+            },
+        },
+        author: {
             type: Schema.Types.ObjectId,
-            ref: "Review",
+            ref: "User",
         },
-    ],
+        reviews: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Review",
+            },
+        ],
+    },
+    opts
+);
+
+// Nesting objects
+CampgroundSchema.virtual("properties.popupMarkup").get(function () {
+    return `<strong><a href='/campgrounds/${this._id}'>${this.title}</a></strong>
+    <p>${this.description.substring(0, 30)}...</p>`;
 });
 
 CampgroundSchema.post("findOneAndDelete", async function (campground) {
